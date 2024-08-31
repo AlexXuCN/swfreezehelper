a34868d829114e4997e8ff30867ea5e9baf430e6
AlexXuCN <alexxucn0@Outlook.com>
2024-08-31 14:52:27 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# swfreezehelper
+
+一键启动旧版的`SeewoFreezeUI.exe --startup-with-main-window`，更方便地管理希沃管家的冰点还原
+
+# 使用
+
+[从release下载](https://github.com/AlexXuCN/swfreezehelper/releases)或[自行编译](#编译)，双击启动即可
+
+# 编译
+
+请准备好旧版的`SeewoFreezeUI.exe`（浅蓝色图标的），放在src下面，再使用`cargo build -r`
+
+> 在哪找？我也不知道
---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use std::path::PathBuf;
+
+fn main() {
+    let seewo_freeze_ui_bin = include_bytes!("SeewoFreezeUI.exe");
+    let mut working_dir = std::env::temp_dir();
+    working_dir.push("swfreezehelper");
+
+    if working_dir.exists() {
+        println!("删除上次操作的临时文件...");
+        std::fs::remove_dir_all(working_dir.clone()).expect("错误：无法删除文件");
+    };
+
+    let mut seewo_service_dir: PathBuf = [r"C:\", "Program Files (x86)", "Seewo", "SeewoService"]
+        .iter()
+        .collect();
+    seewo_service_dir.push({
+        let seewo_service_ver_regex = regex::Regex::new(r"^SeewoService_[0-9]+").unwrap();
+        let mut seewo_service_ver_dirname: String = String::new();
+        for entry in std::fs::read_dir(&(seewo_service_dir)).unwrap() {
+            if seewo_service_ver_regex
+                .is_match(entry.as_ref().unwrap().file_name().to_str().unwrap())
+            {
+                seewo_service_ver_dirname = entry.unwrap().file_name().into_string().unwrap();
+            }
+        }
+        seewo_service_ver_dirname
+    });
+
+    println!("准备中...");
+    {
+        let mut seewo_service_freeze_dir = seewo_service_dir.clone();
+        seewo_service_freeze_dir.push("SeewoFreeze");
+        copy_dir::copy_dir(seewo_service_freeze_dir, working_dir.clone())
+            .expect("错误：无法复制文件");
+    };
+    {
+        let mut seewo_service_crashsdk = seewo_service_dir.clone();
+        seewo_service_crashsdk.push("CrashSDK.dll");
+        let mut working_dir_crashsdk = working_dir.clone();
+        working_dir_crashsdk.push("CrashSDK.dll");
+        std::fs::copy(seewo_service_crashsdk, working_dir_crashsdk)
+            .expect("错误：无法复制文件 CrashSDK.dll");
+    };
+    let mut seewo_service_freezeui = working_dir.clone();
+    seewo_service_freezeui.push("SeewoFreezeUI.exe");
+    std::fs::write(seewo_service_freezeui.clone(), seewo_freeze_ui_bin)
+        .expect("错误：无法写入文件 SeewoFreezeUI.exe");
+
+    let _ = std::process::Command::new(seewo_service_freezeui)
+        .arg("--startup-with-main-window")
+        .spawn();
+}
